00d28b1f35de4dba9c3edda609452948d98f2ef6
kaledenriquez <kaledtrejo@hotmail.com>
2024-05-12 22:21:49 -0600

    home game

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import styled from "styled-components";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import React, { Fragment } from "react";
+
 export function Home() {
+
+const unityProvider =
+  useUnityContext({
+    loaderUrl: "build/myunityapp.loader.js",
+    dataUrl: "build/myunityapp.data",
+    frameworkUrl: "build/myunityapp.framework.js",
+    codeUrl: "build/myunityapp.wasm",
+  });
+
   return (<Container>
-<h1>Home</h1>
+    <Fragment>
+      <Unity unityProvider={unityProvider} style={{ height: 560, width: 1000 }} />
+
+    </Fragment>
   </Container>);
 }
 const Container =styled.div`
