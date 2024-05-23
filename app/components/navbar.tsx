93d3cf00565e21fa20619bffc7b5788fa82e37b8
Skalitz <a01198666@tec.mx>
2024-05-23 10:42:35 -0600

    responsive sidebar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,40 +1,66 @@
+'use client';
+
 import styles from '@/styles/navbar.module.css';
 import Link from 'next/link';
+import { AiOutlineMenu, AiOutlineCloseCircle  } from "react-icons/ai";
+import { useState } from 'react';
+
 import {
   SignInButton,
   SignedIn,
   SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+  UserButton,
+} from '@clerk/nextjs';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen); 
+  };
 
   return (
     <div className={styles.navbar}>
       <div className={styles.stack}>
-        <Link className={styles.bigText} href={"/"}>Learning Limbo</Link>
+        <Link className={styles.bigText} href="/">Learning Limbo</Link>
         <div className={styles.navlinks}>
-          <Link className={styles.smallText} href={"/"}>Home</Link>
-          <Link className={styles.smallText} href={"/quizzes"}>Quizzes</Link>
-          <Link className={styles.smallText} href={"/students"}>Students</Link>
-          <Link className={styles.smallText} href={"/createQuestion"}>Create Question</Link>
-          <Link className={styles.smallText} href={"/statistics"}>Statistics</Link>
+          <Link className={styles.navlink} href="/">Home</Link>
+          <Link className={styles.navlink} href="/quizzes">Quizzes</Link>
+          <Link className={styles.navlink} href="/students">Students</Link>
+          <Link className={styles.navlink} href="/createQuestion">Create Question</Link>
+          <Link className={styles.navlink} href="/statistics">Statistics</Link>
         </div>
 
-        <div>
-            <SignedOut >
-              <SignInButton>
-                <button className={styles.accountButton}>Sign in</button>
-              </SignInButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+        <div className={styles.menu}>
+          <SignedOut>
+            <SignInButton>
+              <button className={styles.accountButton}>Sign in</button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+
+          <AiOutlineMenu className={styles.menuIcon} onClick={handleClick} /> 
+
+          <div className={`${styles.mobileNav} ${isOpen ? styles.active : ''}`}> 
+          <div style={{display: 'flex', justifyContent: 'flex-end', width: '100%'}}>
+            <AiOutlineCloseCircle  className={styles.closeIcon} onClick={handleClick} /> 
+          </div>
+          <SignedOut>
+            <SignInButton>
+              <h1 className={styles.navlink}>Sign in</h1>
+            </SignInButton>
+          </SignedOut>
+            <Link className={styles.navlink} href="/">Home</Link>
+            <Link className={styles.navlink} href="/quizzes">Quizzes</Link>
+            <Link className={styles.navlink} href="/students">Students</Link>
+            <Link className={styles.navlink} href="/createQuestion">Create Question</Link>
+            <Link className={styles.navlink} href="/statistics">Statistics</Link>
+          </div>
+          
         </div>
       </div>
-
-
-
     </div>
-  )
+  );
 }
