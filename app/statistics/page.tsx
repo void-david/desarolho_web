fc15674fe9fd125c0127e9c3119076f375006556
David A. Lozano <void9david@gmail.com>
2024-05-23 07:59:47 -0600

    idea de los cuadros

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styles from './styles.module.css'
 
 function statistics() {
   return (
-    <div>statistics</div>
+    
+    <div className={styles.grid}>
+      <div className={styles.box}>1</div>
+      <div className={styles.box}>2</div>
+      <div className={styles.box}>3</div>
+      <div className={styles.box}>4</div>
+      <div className={styles.box}>5</div>
+      <div className={styles.box}>6</div>
+      <div className={styles.box}>7</div>
+      <div className={styles.box}>8</div>
+      <div className={styles.box}>9</div>
+    </div>
+    
   )
 }
 
