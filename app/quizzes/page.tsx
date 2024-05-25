74b2b3305d55cdbfb3fcaabafde00ce30db9fe11
Kaled Enríquez <kaledtrejo@hotmail.com>
2024-05-25 17:16:59 -0600

    quizzes page

--- app/quizzes/page.tsx
+++ app/quizzes/page.tsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import styles from '@/styles/quizzes.module.css'
+import Link from 'next/link'
+
 
 function quizzes() {
   return (
-    <div>quizzes</div>
+    <>
+
+    <div className={styles.cardContainer}>
+      <Link style={{textDecoration: 'none'}} href="/quizzes/game">
+        <div className={styles.card}>
+
+          <div className={styles.cardImage}></div>
+          <div className={styles.cardContent}>
+            <p>Quiz 1</p>
+            <p>Some description about the quiz</p>
+          </div>
+
+
+        </div>
+      </Link> 
+
+      <Link style={{textDecoration: 'none'}} href="/quizzes/game">
+        <div className={styles.card}>
+
+          <div className={styles.cardImage}></div>
+          <div className={styles.cardContent}>
+            <p>Quiz 2</p>
+            <p>Some description about the quiz</p>
+          </div>
+
+
+        </div>
+      </Link> 
+
+      <Link style={{textDecoration: 'none'}} href="/quizzes/game">
+        <div className={styles.card}>
+
+          <div className={styles.cardImage}></div>
+          <div className={styles.cardContent}>
+            <p>Quiz 3</p>
+            <p>Some description about the quiz</p>
+          </div>
+
+
+        </div>
+      </Link> 
+
+    </div>
+    </>
   )
 }
 
