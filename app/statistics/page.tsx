9e6fdfb03703443a25fffe94e6d789eda7045e7c
Skalitz <a01198666@tec.mx>
2024-05-27 08:53:33 -0600

    statistics

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,20 +1,214 @@
+'use client';
+
 import React from 'react'
-import styles from './styles.module.css'
+import styles from '@/styles/statistics.module.css'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar, Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement, 
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
 
 function statistics() {
   return (
     
-    <div className={styles.grid}>
-      <div className={styles.box}>1</div>
-      <div className={styles.box}>2</div>
-      <div className={styles.box}>3</div>
-      <div className={styles.box}>4</div>
-      <div className={styles.box}>5</div>
-      <div className={styles.box}>6</div>
-      <div className={styles.box}>7</div>
-      <div className={styles.box}>8</div>
-      <div className={styles.box}>9</div>
-    </div>
+    <>
+    
+      <div className={styles.container}>
+        <div className={styles.statsContainer}>
+          <div className={styles.stats}>
+            <h1>Statistics</h1>
+            <Bar
+              data={{
+                labels: ['January', 'February', 'March'],
+                datasets: [
+                {
+                  label: 'My First Dataset',
+                  data: [65, 59, 80],
+                  backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+                {
+                  label: "Loss",
+                  data: [100,200,300],
+                  backgroundColor: 'rgba(49,0,185,0.5)',
+                  
+                },
+
+              ],
+
+              }}
+            />
+          </div>
+        </div>
+
+
+        <div className={styles.statsContainer}>
+          <div className={styles.stats}>
+            <h1>Statistics</h1>
+            <Line
+              data={{
+                labels: ['January', 'February', 'March'],
+                datasets: [
+                {
+                  label: 'My First Dataset',
+                  data: [65, 59, 80],
+                  backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+                {
+                  label: "Loss",
+                  data: [100,200,300],
+                  backgroundColor: 'rgba(49,0,185,0.5)',
+                  
+                },
+
+              ],
+
+              }}
+            />
+          </div>
+        </div>
+
+
+        <div className={styles.statsContainer}>
+          <div className={styles.stats}>
+            <h1>Statistics</h1>
+            <Bar
+              data={{
+                labels: ['January', 'February', 'March'],
+                datasets: [
+                {
+                  label: 'My First Dataset',
+                  data: [65, 59, 80],
+                  backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+                {
+                  label: "Loss",
+                  data: [100,200,300],
+                  backgroundColor: 'rgba(49,0,185,0.5)',
+                  
+                },
+
+              ],
+
+              }}
+            />
+          </div>
+        </div>
+
+
+        <div className={styles.statsContainer}>
+          <div className={styles.stats}>
+            <h1>Statistics</h1>
+            <Bar
+              data={{
+                labels: ['January', 'February', 'March'],
+                datasets: [
+                {
+                  label: 'My First Dataset',
+                  data: [65, 59, 80],
+                  backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+                {
+                  label: "Loss",
+                  data: [100,200,300],
+                  backgroundColor: 'rgba(49,0,185,0.5)',
+                  
+                },
+
+              ],
+
+              }}
+            />
+          </div>
+        </div>
+
+
+        <div className={styles.statsContainer}>
+          <div className={styles.stats}>
+            <h1>Statistics</h1>
+            <Bar
+              data={{
+                labels: ['January', 'February', 'March'],
+                datasets: [
+                {
+                  label: 'My First Dataset',
+                  data: [65, 59, 80],
+                  backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+                {
+                  label: "Loss",
+                  data: [100,200,300],
+                  backgroundColor: 'rgba(49,0,185,0.5)',
+                  
+                },
+
+              ],
+
+              }}
+            />
+          </div>
+        </div>
+
+        <div className={styles.statsContainer}>
+          <div className={styles.stats}>
+            <h1>Statistics</h1>
+            <Bar
+              data={{
+                labels: ['January', 'February', 'March'],
+                datasets: [
+                {
+                  label: 'My First Dataset',
+                  data: [65, 59, 80],
+                  backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+                {
+                  label: "Loss",
+                  data: [100,200,300],
+                  backgroundColor: 'rgba(49,0,185,0.5)',
+                  
+                },
+
+              ],
+
+              }}
+            />
+          </div>
+        </div>
+
+      </div>
+
+    </>
     
   )
 }
