eb870c391cd1dee0de179d7ce0ee30dd1e9f03b9
Fernando Tenchipe Morales <a01571277@tec.mx>
2024-05-29 22:11:45 -0600

    modificacion de ia

--- app/ML/page.tsx
+++ app/ML/page.tsx
@@ -1,34 +1,35 @@
-// pages/generate-story.js
 'use client';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-export default function GenerateStory() {
-  const [story, setStory] = useState('');
+const Page = () => {  
+  const [query, setQuery] = useState('');
+  const [result, setResult] = useState('');
+  
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setQuery(e.target.value);
+  };
 
-  useEffect(() => {
+  const handleKey = async () => {
     const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_API_KEY);
 
-    async function fetchStory() {
-      try {
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        const prompt = "Write a story about a magic backpack.";
-        const result = await model.generateContent(prompt);
-        const response = await result.response;
-        const text = await response.text();
-        setStory(text);
-      } catch (error) {
-        console.error('Error generating story:', error);
-      }
+    try {
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+      const res = await model.generateContent(query);
+      const response = await res.response;
+      const text = await response.text();
+      setResult(text);
+    } catch (error) {
+      console.error('Error generating content:', error);
     }
-
-    fetchStory();
-  }, []);
-
-  return (
+  };
+  
+  return(
     <div>
-      <h1>Generated Story</h1>
-      <p>{story}</p>
+      <textarea name ="" id=""cols={50} rows ={10} onChange={handleChange} />
+      <button onClick={handleKey}>Submit</button>
+      <div>{result}</div>
     </div>
   );
-}
+};
+export default Page
