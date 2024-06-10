f6e331a353adc084bc6468546e7d777b92bf9fb8
David A. Lozano <void9david@gmail.com>
2024-06-09 21:33:50 -0600

    added api and db table for answers

--- server/api/answersApi.js
+++ server/api/answersApi.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const db = require('../db'); // adjust the path according to your project structure
 
 router.post('/api/post-answers', (req, res) => {
-    const {numCorrect, numIncorrect} = req.body;
+    const {certainty, numCorrect, numIncorrect, score} = req.body;
     
-    const sql = `INSERT INTO Answers (userId, numCorrect, numIncorrect) VALUES (?, ?, ?)`;
+    const sql = `INSERT INTO Answers (certainty, numCorrect, numIncorrect, score) VALUES (?, ?, ?, ?)`;
     
-    db.run(sql, [1, numCorrect, numIncorrect], function(err) {
+    db.run(sql, [certainty, numCorrect, numIncorrect, score], function(err) {
         if (err) {
         return res.status(500).send(err.message);
         }
